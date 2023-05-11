0ea449006b48f5e7ef31c8ccf96f3f318a0b2149
Aleksey Putilov <a1eksey.bru@yandex.ru>
2023-05-11 20:51:02 +0300

    Средний кластерный коэффициент + обобщение двудольных и не двудольных
    Co-Authored-By: Egor Prokofev <egor.prokofev.ru2th@gmail.com>
    Co-Authored-By: tottttttt <86297881+tottttttt@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import numpy as np
-data = pd.read_csv('4.tsv', sep = "\t", header= None, names=['in','out','weight','time'])
-dvudol = True
+import math
+file = '7.tsv'
+data = pd.read_csv(file, sep = "\t", header= None, names=['in','out','weight','time'])
+if math.isnan(data.iloc[0]['out']):
+    data = pd.read_csv(file, sep=" ", header=None, names=['in', 'out', 'weight', 'time'])
+dvudol = False
+AllVertex = {}
 #Вывести количество ребер
 print('Количество ребер в графе: ' + str(data.shape[0]))
 
@@ -18,53 +23,71 @@ if dvudol:
                 left.append((str(row['out']) + 'r'))
                 LeftVertex[row['in']] = left
         else:
-            LeftVertex[row['in']] = [(str(row['out']) + 'r')]
+            LeftVertex[row['in']] = [(str(row['out'])) + 'r']
 
-        right = RightVertex.get((str(row['out']) + 'r'))
+        right = RightVertex.get(str(row['out']) + 'r')
         if right:
             if row['in'] in right:
                 continue
             else:
                 right.append(row['in'])
-                RightVertex[(str(row['out']) + 'r')] = right
+                RightVertex[str(row['out']) + 'r'] = right
         else:
-            RightVertex[(str(row['out']) + 'r')] = [row['in']]
+            RightVertex[str(row['out']) + 'r'] = [row['in']]
     AllVertex = LeftVertex | RightVertex
     countVertex = len(AllVertex)
 else:
-    pass
+    for index, row in data.iterrows():
+        left = AllVertex.get(row['in'])
+        if left:
+            if (row['out']) in left:
+                continue
+            else:
+                left.append(row['out'])
+                AllVertex[row['in']] = left
+        else:
+            AllVertex[row['in']] = [row['out']]
+
+        right = AllVertex.get(row['out'])
+        if right:
+            if row['in'] in right:
+                continue
+            else:
+                right.append(row['in'])
+                AllVertex[row['out']] = right
+        else:
+            AllVertex[row['out']] = [row['in']]
+    countVertex = len(AllVertex)
 
 print('Количество вершин в графе: ' + str(countVertex))
 
 #Считаем плотность графа
-print("Плотность графа: " + str(data.shape[0]/(len(RightVertex)*len(LeftVertex))))
+if dvudol:
+    print("Плотность графа: " + str(data.shape[0]/(len(RightVertex)*len(LeftVertex))))
+else:
+    print("Плотность графа: " + str(data.shape[0] / (countVertex*(countVertex-1)*0.5)))
 
 #Считаем количество Компонент слабой связности
 
-def DFS_for_weak_component(vertex, part, component):
+def DFS_for_weak_component(vertex, component):
     visited.add(vertex)
     component.append(vertex)
-    if part:
-        neighs = LeftVertex.get(vertex)
-    else:
-        neighs = RightVertex.get(vertex)
+    neighs = AllVertex.get(vertex)
     for neigh in neighs:
         if neigh in visited:
             continue
-        elif part:
-            DFS_for_weak_component(neigh, False, component)
-        else:
-            DFS_for_weak_component(neigh, True, component)
+        DFS_for_weak_component(neigh, component)
+
 
 visited = set([])
 components = []
-for vertex in LeftVertex.items():
+for vertex in AllVertex.items():
     if len(visited) == countVertex:
         break
     if vertex[0] in visited:
         continue
     component = []
-    DFS_for_weak_component(vertex[0], True, component)
+    DFS_for_weak_component(vertex[0], component)
     components.append(component)
 print('Количество компонент слабой связности: ' + str(len(components)))
 
@@ -72,7 +95,7 @@ print('Количество компонент слабой связности: 
 print('Доля вершин в максимальной по мощности компоненте слабой связности: ' + \
 str(len(max(components, key=len))/countVertex))
 
-#Ищем радиус и диаметр
+#Ищем радиус и диаметр и 90 процентиль
 import random
 from collections import deque
 max_component = max(components, key=len)
@@ -84,14 +107,11 @@ def BFS_for_snow_subgraph(vertex):
     while len(queue)>0 and len(subgraph)<750:
         actual = queue.popleft()
         subgraph.append(actual)
-        neighs = []
-        if str(actual)[-1]!='r':
-            neighs = LeftVertex.get(actual)
-        else:
-            neighs = RightVertex.get(actual)
+        neighs = AllVertex.get(actual)
         for neigh in neighs:
             if neigh not in visited:
                 queue.append(neigh)
+                visited.add(neigh)
     return subgraph
 
 def simple_search(max_component):
@@ -102,10 +122,7 @@ def simple_search(max_component):
         order_in_max_component[vertex] = i
     for i in range(len(max_component)):
         vertex = max_component[i]
-        if str(vertex)[-1]!='r':
-            neighs = LeftVertex.get(vertex)
-        else:
-            neighs = RightVertex.get(vertex)
+        neighs = AllVertex.get(vertex)
         for neigh in neighs:
             j = order_in_max_component.get(neigh)
             if j is not None:
@@ -144,7 +161,27 @@ else:
     simple_search(max_component)
 
 #Вычисляем средний кластерный коэффициент:
+def Cl_calc():
+    Cl = 0
+    for vertex in AllVertex.items():
+        Cli = 0
+        if len(vertex[1]) < 2:
+            continue
+        for item in vertex[1]:
+            neigh_list = AllVertex.get(item)
+            for neigh_list_item in neigh_list:
+                if neigh_list_item==vertex[0]:
+                    continue
+                if neigh_list_item in vertex[1]:
+                    Cli+=1
+        Cl += Cli/(len(vertex[1])*(len(vertex[1])-1))
+    Cl /= countVertex
+    return Cl
 
+if dvudol:
+    print('Средний кластерный коэффициент: ' + str(Cl_calc()))
+else:
+    print('Средний кластерный коэффициент: ' + str(Cl_calc()))
 
 
 
