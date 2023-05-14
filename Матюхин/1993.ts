ca6bfed20e4ca454417dbe8c97007609f6fade09
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2023-05-14 03:03:38 +0300

    feat: add the submission 1 for the 1993 problem

---
+++ Матюхин/1993.ts
@@ -0,0 +1,85 @@
+interface LockingTreeNode {
+    childNumbers: number[];
+    lockedBy: number | null;
+    parentNumber: number;
+}
+
+class LockingTree {
+    private nodes: LockingTreeNode[];
+
+    constructor(parentNumbers: number[]) {
+        this.nodes = parentNumbers.reduce((
+            nodes: LockingTreeNode[],
+            parentNumber: number,
+            nodeNumber: number,
+        ) => {
+            if (parentNumber !== -1) {
+                nodes[parentNumber].childNumbers.push(nodeNumber);
+            }
+
+            return [...nodes, {
+                parentNumber,
+                childNumbers: [],
+                lockedBy: null,
+            }];
+        }, []);
+    }
+
+    lock(nodeNumber: number, user: number): boolean {
+        const node = this.nodes[nodeNumber];
+
+        if (node.lockedBy !== null) {
+            return false;
+        }
+    
+        node.lockedBy = user;
+        return true;
+    }
+
+    unlock(nodeNumber: number, user: number): boolean {
+        const node = this.nodes[nodeNumber];
+
+        if (node.lockedBy !== user) {
+            return false;
+        }
+        
+        node.lockedBy = null;
+        return true;
+    }
+
+    upgrade(nodeNumber: number, user: number): boolean {
+        let currentNodeNumber = nodeNumber;
+
+        while (currentNodeNumber !== -1) {
+            const currentNode = this.nodes[currentNodeNumber];
+
+            if (currentNode.lockedBy !== null) {
+                return false;
+            }
+
+            currentNodeNumber = currentNode.parentNumber;
+        }
+
+        if (!this.isAnyChildLocked(nodeNumber)) {
+            return false;
+        }
+
+        this.unlockAllChildren(nodeNumber);
+        this.nodes[nodeNumber].lockedBy = user;
+        return true;
+    }
+
+    private isAnyChildLocked(nodeNumber: number): boolean {
+        return this.nodes[nodeNumber].childNumbers.some((childNumber) => {
+            return this.nodes[childNumber].lockedBy !== null ||
+                this.isAnyChildLocked(childNumber);
+        });
+    }
+
+    private unlockAllChildren(nodeNumber: number): void {
+        this.nodes[nodeNumber].childNumbers.forEach((childNumber) => {
+            this.nodes[childNumber].lockedBy = null;
+            this.unlockAllChildren(childNumber);
+        });
+    }
+}
