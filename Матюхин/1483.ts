d2c5be10ad69cd2dd60e86a3427e4937a048a696
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2023-05-16 19:35:32 +0300

    feat: add the submission 1 for the 1483 problem

---
+++ Матюхин/1483.ts
@@ -0,0 +1,29 @@
+class TreeAncestor {
+    private sparseTable: number[][] = [];
+
+    constructor(_: number, parentNumbers: number[]) {
+        this.sparseTable[0] = [...parentNumbers];
+        let isSparseTableFilled = false;
+
+        for (let i = 1; !isSparseTableFilled; ++i) {
+            this.sparseTable[i] = new Array(parentNumbers.length).fill(-1);
+            isSparseTableFilled = true;
+
+            for (let j = 0; j < parentNumbers.length; ++j) {
+                const prevAncestor = this.sparseTable[i - 1][j];
+
+                if (prevAncestor !== -1) {
+                    this.sparseTable[i][j] = parentNumbers[prevAncestor];
+
+                    if (this.sparseTable[i][j] !== -1) {
+                        isSparseTableFilled = false;
+                    }
+                }
+            }
+        }
+    }
+
+    getKthAncestor(node: number, k: number): number {
+        return k === 0 ? node : this.sparseTable[k - 1][node];
+    }
+}
