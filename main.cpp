23a93d93593ed00a376cab89a53ba7aba354ddc0
kamyn <kamynin.pavel.74@gmail.com>
2023-05-23 02:17:57 +0300

    StaticGraph task 1.1 updated

--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 #include <fstream>
 #include <vector>
 #include <unordered_set>
+#include <queue>
 
 class StaticGraph
 {
-private: 
+private:
+    int _vertexCount = 0;
+    int _edgeCount = 0;
+    int _mainCompCount = 0;
     std::vector<std::unordered_set<int>> _staticGraph;
+    std::vector<std::vector<int>> _weakComponents;
 public:  
     void SetVertex(int n){
         _staticGraph.resize(n+1); 
@@ -19,12 +24,56 @@ public:
         }
     }
 
-    //mock
-    int GetVertexCount(){ return 0; }
-    int GetEdgeCount(){ return 0; }
-    int GetDensity(){ return 0; }
-    int GetWeakConnCount(){ return 0; }
-    int GetMainCompToFull(){ return 0; }
+    int GetVertexCount(){
+        return _staticGraph.size() - 1;
+    }
+
+    int GetEdgeCount(){
+        if (_edgeCount == 0){
+            for (auto v: _staticGraph)
+                _edgeCount += v.size();
+        }
+        return _edgeCount / 2;
+    }
+
+    double GetDensity(){
+        return (double)GetEdgeCount() / ((GetVertexCount() * (GetVertexCount() - 1)) / 2);
+    }
+
+    int GetWeakConnCount(){
+        int n = GetVertexCount();
+        std::vector<bool> visited(n+1);
+        std::queue<int> bfs;
+        int comp = 0;
+        for(int i = 1; i <= n; ++i){
+            if (!visited[i]){
+                bfs.push(i);
+                _weakComponents.push_back({i});
+                visited[i] = true;
+                while(!bfs.empty()){
+                    int front = bfs.front();
+                    bfs.pop();
+                    for (auto v: _staticGraph[front]){
+                        if (!visited[v]){
+                            bfs.push(v);
+                            _weakComponents[comp].push_back(v);
+                            visited[v] = true;
+                        }
+                    }
+                }
+                ++comp;
+            }
+        }
+        return comp;
+    }
+
+    double GetMainCompToFull(){
+        int max = 0;
+        for (int i = 0; i < _weakComponents.size(); ++i)
+            if (_weakComponents[i].size() > max)
+                max = _weakComponents[i].size();
+        return (double)max / GetVertexCount();
+    }    
 };
 
 bool DataReader(std::string fileName, StaticGraph& graph){
