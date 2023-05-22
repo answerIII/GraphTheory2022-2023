492d5675b2e42e8bad4d9265b45a6b11ee8a1b3c
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2023-05-22 05:30:01 +0300

    feat: add upload, make alerts functional

--- public/index.js
+++ public/index.js
@@ -1,7 +1,16 @@
 // @ts-check
 
+/**
+ * @typedef STLPGraphEdge An edge of a graph, with additional attached data.
+ * @property {number} to An edge destination.
+ * @property {number} weight An edge weight.
+ * @property {number} time An edge creation time.
+ */
+
+/** @typedef {Set<STLPGraphEdge>[]} STLPGraph A graph as an edge list. */
+
 /** A custom element. */
-class GraphElement extends HTMLElement {
+class STLPElement extends HTMLElement {
   /**
    * Attaches `template` as a shadow DOM tree to this element.
    * @param {HTMLTemplateElement} template The template element to attach.
@@ -14,32 +23,101 @@ class GraphElement extends HTMLElement {
 }
 
 /** A custom alert element. */
-class GraphAlertElement extends GraphElement {
+class STLPAlertElement extends STLPElement {
   constructor() {
     super();
 
     this.attachTemplate(
       /** @type {HTMLTemplateElement} */ (
-        document.getElementById("graph-alert-template")
+        document.getElementById("stlp-alert-template")
       )
     );
   }
 }
 
 customElements.define(
-  "graph-error-alert",
+  "stlp-error-alert",
   /** A custom error alert element. */
-  class GraphErrorAlertElement extends GraphAlertElement {}
+  class STLPErrorAlertElement extends STLPAlertElement {}
 );
 
 customElements.define(
-  "graph-info-alert",
+  "stlp-info-alert",
   /** A custom information alert element. */
-  class GraphInfoAlertElement extends GraphAlertElement {}
+  class STLPInfoAlertElement extends STLPAlertElement {}
 );
 
 customElements.define(
-  "graph-warn-alert",
+  "stlp-warn-alert",
   /** A custom warning alert element. */
-  class GraphWarnAlertElement extends GraphAlertElement {}
+  class STLPWarnAlertElement extends STLPAlertElement {}
 );
+
+/**
+ * Parses `datasetFile` and returns the graph from it.
+ * @param {File} datasetFile The uploaded file with the data.
+ * @returns {Promise<STLPGraph>} The graph as the edge list.
+ */
+const stlpParseDataset = async (datasetFile) => {
+  const graph = [];
+  const textDecoder = new TextDecoder();
+  let line = "";
+
+  // @ts-ignore 2504 It has the async iterator.
+  for await (const chunk of datasetFile.stream()) {
+    const data = textDecoder.decode(chunk);
+
+    for (const character of data) {
+      if (character !== "\r" && character !== "\n") {
+        line += character;
+        continue;
+      }
+
+      if (line !== "" && !line.startsWith("%")) {
+        // TODO: Parse the line (maybe no line accumulator?)
+      }
+
+      line = "";
+    }
+  }
+
+  return graph;
+};
+
+/**
+ * Shows the alert of `type` with `text`.
+ * @param type {string} The alert type (error, info or warn).
+ * @param text {string} The text to display on the alert.
+ */
+const stlpShowAlert = (type, text) => {
+  const textElement = document.createElement("span");
+  textElement.slot = "text";
+  textElement.textContent = text;
+
+  const hostElement = document.createElement(`stlp-${type}-alert`);
+  hostElement.appendChild(textElement);
+  document.body.appendChild(hostElement);
+
+  setTimeout(HTMLElement.prototype.remove.bind(hostElement), 2500);
+};
+
+const datasetForm = /** @type {HTMLFormElement} */ (
+  document.getElementById("dataset-form")
+);
+
+datasetForm.addEventListener("submit", (event) => {
+  console.info("The dataset is submitted");
+  event.preventDefault();
+
+  const datasetForm = /** @type {HTMLFormElement} */ (event.target);
+
+  const datasetFile = /** @type {File} */ (
+    new FormData(datasetForm).get("dataset")
+  );
+
+  if (datasetFile.size === 0) {
+    stlpShowAlert("error", "The dataset is empty");
+  } else {
+    stlpParseDataset(datasetFile);
+  }
+});
