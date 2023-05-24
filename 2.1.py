fc1355e460b46e2b080a7c5f47588545f7cc8146
Andrew55529 <lashkov8@gmail.com>
2023-05-24 22:41:13 +0300

    Add AUC curve plot

--- 2.1.py
+++ 2.1.py
@@ -117,3 +117,66 @@ for pair in negatives:
     PA = preferential_attachment(pair[0], pair[1], adjacency_list)
     X.append([CN, AA, JC, PA])
     Y.append(0)
+
+    
+    
+# Получения списка ребер
+edges_r = dict({})
+for row in data.itertuples():
+    if (row[1]<row[2]):
+        fir=row[1]
+        sec=row[2]
+    else:
+        fir = row[2]
+        sec = row[1]
+    if (fir, sec) in edges_r:
+        edges_r[(fir, sec)].append(row[4])
+    else:
+        edges_r[(fir, sec)] = [row[4]]
+
+ver_count=len(adjacency_list)
+print(ver_count)
+
+s=(tmax-tmin)/2
+print(tmin,tmax,s)
+
+# Все ребра которых небыло до s
+edges_p=[]
+edges_n=[]
+for i in range(1, ver_count):
+    for j in range(i+1, ver_count):
+        if (j in adjacency_list[i]):
+            if (min(edges_r[(i,j)])>=s):
+                edges_p.append((i, j))
+        else:
+            edges_n.append((i,j))
+# Ограничить расстоянием
+
+# Заменить ребра на
+for i in range(len(edges_p)):
+    CN = common_neighbours(edges_p[i][0], edges_p[i][1], adjacency_list)
+    AA = adamic_adar(edges_p[i][0], edges_p[i][1], adjacency_list)
+    JC = jaccard_coefficient(edges_p[i][0], edges_p[i][1], adjacency_list)
+    PA = preferential_attachment(edges_p[i][0], edges_p[i][1], adjacency_list)
+    edges_p[i]=[CN, AA, JC, PA]
+
+for i in range(len(edges_n)):
+    CN = common_neighbours(edges_n[i][0], edges_n[i][1], adjacency_list)
+    AA = adamic_adar(edges_n[i][0], edges_n[i][1], adjacency_list)
+    JC = jaccard_coefficient(edges_n[i][0], edges_n[i][1], adjacency_list)
+    PA = preferential_attachment(edges_n[i][0], edges_n[i][1], adjacency_list)
+    edges_n[i]=[CN, AA, JC, PA]
+
+X_new=edges_n + edges_p;
+Y_new=[0]*len(edges_n)+[1]*len(edges_p);
+# Делим 25% тестовых
+model = LogisticRegression()
+X_train, X_test, Y_train, Y_test = train_test_split(X_new, Y_new, test_size=0.25, random_state=0)
+# Обучение
+model.fit(X_train, Y_train)
+accuracy = accuracy_score(Y_test, model.predict(X_test))
+auc_roc = roc_auc_score(Y_test, model.predict_proba(X_test)[:, 1])
+print(classification_report(Y_test, model.predict(X_test)))
+svc_disp = RocCurveDisplay.from_estimator(model, X_test, Y_test)
+plt.show()
+    
