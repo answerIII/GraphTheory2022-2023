0002d154dda3dbb1888cd4ce1a1b357ff4215557
Alina Smolina <78797567+Alina-Smol@users.noreply.github.com>
2023-05-25 14:52:04 +0300

    Update temporal_features.py

--- temporal_features.py
+++ temporal_features.py
@@ -4,170 +4,188 @@ from collections import defaultdict
 import networkx as nx
 
 # A 
+def calc_temporate_feauters(dataset, qs):
 
-def temporal_weight(t_arr, t_min, t_max) -> (dict):
+  def temporal_weight(t_arr, t_min, t_max) -> (dict):
 
-  t_weight = defaultdict(list)
+    t_weight = defaultdict(list)
 
-  l = 0.2 
-  t_minmax = t_max - t_min
-  for t in t_arr: 
+    l = 0.2
+    t_minmax = t_max - t_min
+    for t in t_arr:
 
-    tt = (t-t_min)/(t_minmax) 
+      tt = (t-t_min)/(t_minmax)
 
-    t_weight['linear'].append(l + (1-l)*(tt))
-    t_weight['exponential'].append(l + (1-l)*((math.exp(3*tt) - 1) / (math.exp(3) - 1)))
-    t_weight['square root'].append(l + (1-l)*(tt**0.5))
-
-
-  return t_weight 
-
-# B 
-
-def past_event_aggregation(weight) -> (dict): 
-
-  a = {}
-
-  a['zeroth'], a['first'], a['second'], a['third'], a['fourth quantile'] = np.quantile(weight, [0, 0.25, 0.5, 0.75, 1])
-   
-  a['sum'] = sum(weight) 
-  a['mean'] = np.mean(weight)
-  a['variance'] = np.var(weight) 
-  
-  return a 
-  
-# C 
+      t_weight['linear'].append(l + (1-l)*(tt))
+      t_weight['exponential'].append(l + (1-l)*((math.exp(3*tt) - 1) / (math.exp(3) - 1)))
+      t_weight['square root'].append(l + (1-l)*(tt**0.5))
 
 
-def CN_temporal(u, v, z_arr, graph, number) -> (list): 
-  CN = np.zeros(len(graph[u][v]['weight'][number]))
+    return t_weight
 
-  for z in z_arr: 
-    CN = CN + np.array(graph[u][z]['weight'][number]) + np.array(graph[v][z]['weight'][number])  
+  # B
 
-  return CN 
-  
-def JC_temporal(u, v, z_arr, graph, number) -> list: 
-  n =  len(graph[u][v]['weight'][number])
-  x = np.zeros(n)
-  y = np.zeros(n)
-  JC = np.zeros(n) 
+  def past_event_aggregation(weight) -> (dict):
 
-  for i in graph[u]: 
-    x += np.array(graph[u][i]['weight'][number]) 
+    a = {}
 
-  for i in graph[v]: 
-    y += np.array(graph[v][i]['weight'][number])
+    a['zeroth'], a['first'], a['second'], a['third'], a['fourth quantile'] = np.quantile(weight, [0, 0.25, 0.5, 0.75, 1])
 
-  tmp = x + y 
-  for z in z_arr: 
-    JC = JC + (np.array(graph[u][z]['weight'][number]) + np.array(graph[v][z]['weight'][number])) / tmp 
-  return JC 
+    a['sum'] = sum(weight)
+    a['mean'] = np.mean(weight)
+    a['variance'] = np.var(weight)
 
-# sum([1,2,3], [4,5,6])
+    return a
+
+  # C
 
 
-def PA_temporal(u, v, graph, number) -> (list):
-  n = len(graph[u][v]['weight'][number])
-  PA = np.zeros(n) 
-  x = np.zeros(n)
-  y = np.zeros(n)
+  def CN_temporal(u, v, z_arr, graph, number) -> (list):
+    CN = np.zeros(len(graph[u][v]['weight'][number]))
 
-  for i in graph[u]: 
-    x += np.array(graph[u][i]['weight'][number]) 
+    for z in z_arr:
+      CN = CN + np.array(graph[u][z]['weight'][number]) + np.array(graph[v][z]['weight'][number])
 
-  for i in graph[v]: 
-    y += np.array(graph[v][i]['weight'][number])
-  
-  PA = x * y 
+    return CN
 
-  return PA 
+  def JC_temporal(u, v, z_arr, graph, number) -> list:
+    n =  len(graph[u][v]['weight'][number])
+    x = np.zeros(n)
+    y = np.zeros(n)
+    JC = np.zeros(n)
 
-def AA_temporal(u, v, z_arr, graph, number) -> (list):
-  n =  len(graph[u][v]['weight'][number])
-  AA = np.zeros(n) 
-  sum_x = np.zeros(n)
+    for i in graph[u]:
+      x += np.array(graph[u][i]['weight'][number])
 
-  for z in z_arr:
-    x = graph[z]
+    for i in graph[v]:
+      y += np.array(graph[v][i]['weight'][number])
 
-    for x1 in x:
-      sum_x += np.array(graph[z][x1]['weight'][number])
-    
-    AA = AA + (np.array(graph[u][z]['weight'][number]) + np.array(graph[v][z]['weight'][number])) / np.log(1 + sum_x)
-    # print("AA", AA)
-    # print('log', np.log(1 + sum_x))
-    sum_x = np.zeros(n)  
+    tmp = x + y
+    b = False
+    if tmp[-1] == 0:
+      tmp[-1] = 1 #?????
+      b = True
+    for z in z_arr:
+      JC = JC + (np.array(graph[u][z]['weight'][number]) + np.array(graph[v][z]['weight'][number])) / tmp
+    if b:
+      JC[-1] = 0
+    return JC
 
-  return AA
+  # sum([1,2,3], [4,5,6])
 
-def topological_features(u, v, graph, number) -> list: 
-  u_neigh = set(graph[u]) 
-  v_neigh = set(graph[v]) 
-  z_arr = list(u_neigh & v_neigh) 
-  if len(z_arr) > 0:
-    AA = AA_temporal(u, v, z_arr, graph, number) 
-    CN = CN_temporal(u, v, z_arr, graph, number) 
-    JC = JC_temporal(u, v, z_arr, graph, number)
-  else: 
+
+  def PA_temporal(u, v, graph, number) -> (list):
     n = len(graph[u][v]['weight'][number])
-    AA, CN, JC = [0]*n, [0]*n, [0]*n
+    PA = np.zeros(n)
+    x = np.zeros(n)
+    y = np.zeros(n)
 
-  PA = PA_temporal(u, v, graph, number)
+    for i in graph[u]:
+      x += np.array(graph[u][i]['weight'][number])
 
-  print(AA)
-  print(CN)
-  print(JC)
-  print(PA) 
+    for i in graph[v]:
+      y += np.array(graph[v][i]['weight'][number])
 
-  features = list(AA) + list(CN) + list(JC) + list(PA)
+    PA = x * y
 
-  return features 
+    return PA
 
+  def AA_temporal(u, v, z_arr, graph, number) -> (list):
+    n =  len(graph[u][v]['weight'][number])
+    AA = np.zeros(n)
+    sum_x = np.zeros(n)
 
-f = open('datasets\\small-graph.txt', 'r')
+    for z in z_arr:
+      x = graph[z]
 
-dataset = f.readlines()
-uniqueVertexes = set()
+      for x1 in x:
+        sum_x += np.array(graph[z][x1]['weight'][number])
 
-for line in dataset:
-    [from_ind, to_ind, weight, time] = [int(x) for x in line.split()]
-    uniqueVertexes.add(from_ind)
-    uniqueVertexes.add(to_ind)
+      b = False
 
-ver = defaultdict(list)
-all_time = []
-l = 0.7
-# matrix = defaultdict(list)
-for line in dataset:
-    [from_ind, to_ind, weight, time] = [int(x) for x in line.split()]
-    if ver[(to_ind, from_ind)]:
-        ver[(to_ind, from_ind)].append(time)
+      if sum_x[-1] == 0:
+         sum_x[-1] = 0.0001
+         tmp = AA[-1]
+         b = True
+
+      AA = AA + (np.array(graph[u][z]['weight'][number]) + np.array(graph[v][z]['weight'][number])) / np.log(1 + sum_x)
+      # print("AA", AA)
+      # print('log', np.log(1 + sum_x))
+      sum_x = np.zeros(n)
+      if b:
+        AA[-1] = tmp
+
+    return AA
+
+  def topological_features(u, v, graph, number) -> list:
+    u_neigh = set(graph[u])
+    v_neigh = set(graph[v])
+    z_arr = list(u_neigh & v_neigh)
+    if len(z_arr) > 0:
+      AA = AA_temporal(u, v, z_arr, graph, number)
+      CN = CN_temporal(u, v, z_arr, graph, number)
+      JC = JC_temporal(u, v, z_arr, graph, number)
     else:
-        ver[(from_ind, to_ind)].append(time)
-    all_time.append(time)
+      n = len(graph[u][v]['weight'][number])
+      AA, CN, JC = [0]*n, [0]*n, [0]*n
 
-t_min = min(all_time)
-t_max = max(all_time)
+    PA = PA_temporal(u, v, graph, number)
 
-G = nx.Graph()
+    print(AA)
+    print(CN)
+    print(JC)
+    print(PA)
 
-weight_liner = defaultdict(list)
+    features = list(AA) + list(CN) + list(JC) + list(PA)
 
-for uv in ver:
-  if len(ver[uv]) != 0:
-    w = temporal_weight(ver[uv], t_min, t_max)
-    weight_liner = past_event_aggregation(w['linear'])
-    weight_exponetial = past_event_aggregation(w['exponential'])
-    weight_square = past_event_aggregation(w['square root'])
+    return features
 
-    G.add_edge(uv[0], uv[1], weight = [list(weight_liner.values())])
-    G[uv[0]][uv[1]]['weight'] += [list(weight_exponetial.values())]
-    G[uv[0]][uv[1]]['weight'] += [list(weight_square.values())]
+  ver = defaultdict(list)
+  all_time = []
+  ver_after = []
 
-ver.clear()
-for i in range(3):
-  for j in G.edges:
-    ver[(j[0],j[1])].append(topological_features(j[0], j[1], G, i))
+  for line in dataset:
+      [from_ind, to_ind, weight, time] = [int(x) for x in line.split()]
+      all_time.append(time)
+      if time <= qs:
+        if ver[(to_ind, from_ind)]:
+            ver[(to_ind, from_ind)].append(time)
+        else:
+            ver[(from_ind, to_ind)].append(time)
 
-  
+      else:
+        ver_after.append([from_ind, to_ind])
+
+  t_min = min(all_time)
+  t_max = max(all_time)
+
+  G = nx.Graph()
+
+  weight_liner = defaultdict(list)
+
+  for uv in ver:
+    if len(ver[uv]) != 0:
+      w = temporal_weight(ver[uv], t_min, t_max)
+      weight_liner = past_event_aggregation(w['linear'])
+      weight_exponetial = past_event_aggregation(w['exponential'])
+      weight_square = past_event_aggregation(w['square root'])
+
+      G.add_edge(uv[0], uv[1], weight = [list(weight_liner.values())])
+      G[uv[0]][uv[1]]['weight'] += [list(weight_exponetial.values())]
+      G[uv[0]][uv[1]]['weight'] += [list(weight_square.values())]
+
+  ver.clear()
+  for i in range(3):
+    for j in G.edges:
+      ver[(j[0], j[1])] += topological_features(j[0], j[1], G, i)
+
+  y = [0] * len(ver)
+  j = 0
+  print('ver', len(ver))
+
+  for i in ver:
+    if ([i[0], i[1]] in ver_after) or ([i[1],i[0]] in ver_after):
+      y[j] = 1
+    j += 1
+
+  return ver, y
