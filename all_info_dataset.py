6865c4f0920869964a12bef0da9b71b8ac9f48c5
vanes <73476639+nenakhov22@users.noreply.github.com>
2023-05-26 18:20:25 +0300

    Add files via upload

---
+++ all_info_dataset.py
@@ -0,0 +1,292 @@
+import pandas as pd
+import math
+import random
+import numpy as np
+from collections import deque
+import collections
+
+
+#Получить список смежности для всего графа за все время
+def get_adjacency_list(data):
+    adjacency_list = dict({})
+    edges_r = dict({})
+    time = []
+    count_edges = 0
+    is_loop = False
+    for row in data.itertuples():
+        # Список смежности для все ребер за все время
+        count_add = 0
+        if (row[1] in adjacency_list):
+            if (row[2] not in adjacency_list[row[1]]):
+                adjacency_list[row[1]].append(row[2])
+                count_add += 1
+        else:
+            adjacency_list[row[1]] = [row[2]]
+            count_add += 1
+        if (row[2] in adjacency_list):
+            if (row[1] not in adjacency_list[row[2]]):
+                adjacency_list[row[2]].append(row[1])
+                count_add += 1
+        else:
+            adjacency_list[row[2]] = [row[1]]
+            count_add += 1
+
+        if count_add >= 1:
+            if count_add == 1:
+                is_loop = True
+            count_edges += 1
+
+        # Заносим все ребра
+        if (row[1] < row[2]):
+            fir = row[1]
+            sec = row[2]
+        else:
+            fir = row[2]
+            sec = row[1]
+        if (fir, sec) in edges_r:
+            edges_r[(fir, sec)].append(row[4])
+        else:
+            edges_r[(fir, sec)] = [row[4]]
+
+        time.append(row[4])
+    return [adjacency_list, min(time), max(time), edges_r, count_edges, is_loop]
+
+
+#Получить список смежности для графа до момента s
+def get_adjacency_list_until_s(data, s):
+    adjacency_list_until_s = dict({})
+    edges_r_until_s = dict({})
+    time = []
+    for row in data.itertuples():
+        # Список смежности до момента s
+        if row[4] <= s:
+            if (row[1] in adjacency_list_until_s):
+                if (row[2] not in adjacency_list_until_s[row[1]]):
+                    adjacency_list_until_s[row[1]].append(row[2])
+            else:
+                adjacency_list_until_s[row[1]] = [row[2]]
+            if (row[2] in adjacency_list_until_s):
+                if (row[1] not in adjacency_list_until_s[row[2]]):
+                    adjacency_list_until_s[row[2]].append(row[1])
+            else:
+                adjacency_list_until_s[row[2]] = [row[1]]
+
+            # Заносим все ребра до s
+            if (row[1] < row[2]):
+                fir = row[1]
+                sec = row[2]
+            else:
+                fir = row[2]
+                sec = row[1]
+            if (fir, sec) in edges_r_until_s:
+                edges_r_until_s[(fir, sec)].append(row[4])
+            else:
+                edges_r_until_s[(fir, sec)] = [row[4]]
+
+            time.append(row[4])
+
+    return adjacency_list_until_s, edges_r_until_s, min(time), max(time)
+
+
+def floyd_warshall(adjacency_list):
+    d = [[[] for _ in range(len(adjacency_list))] for __ in range(len(adjacency_list))]
+    for node_1 in adjacency_list:
+        for node_2 in adjacency_list:
+            d[list(adjacency_list).index(node_1)][list(adjacency_list).index(node_2)] = [math.inf, min(node_1, node_2),
+                                                                                         max(node_1, node_2)]
+
+    for node in adjacency_list:
+        for neighbour in adjacency_list[node]:
+            d[list(adjacency_list).index(node)][list(adjacency_list).index(neighbour)][0] = 1
+
+    for i in range(len(d)):
+        d_i = [d[j][:] for j in range(len(d))]
+        for u in range(len(d)):
+            for v in range(len(d)):
+                d[u][v][0] = min(d_i[u][v][0], d_i[u][i][0] + d_i[i][v][0])
+
+    for i in range(len(d)):
+        d[i][i][0] = 0
+    return d
+
+def BFS(node, adjacency_list):
+    visited = set()
+    queue = deque()
+    queue.append(node)
+    visited.add(node)
+    dist = 1
+    while len(queue) > 0 and dist < 3:
+        size = len(queue)
+        while size > 0:
+            cur = queue.popleft()
+            size -= 1
+            for neighbour in adjacency_list[cur]:
+                if (neighbour not in visited):
+                    queue.append(neighbour)
+                    visited.add(neighbour)
+        dist += 1
+    return list(queue)
+
+
+def get_info_dataset(data, q):
+    adjacency_list, tmin, tmax, edges_r, count_edges, is_loop = get_adjacency_list(data)
+    nodesNumber = len(adjacency_list)
+    print(nodesNumber)
+
+    s = (tmax + tmin) * q
+    print('tmin: ' + str(tmin))
+    print('tmax: ' + str(tmax))
+    print('ts: ' + str(s))
+
+    #Нужные данные до момента s
+    adjacency_list_until_s, edges_r_until_s, tmin, tmax = get_adjacency_list_until_s(data, s)
+
+    number1 = 0
+    number0 = 0
+    edges_p = []
+    edges_n = []
+
+    for i in range(1, nodesNumber + 1):
+        for j in range(i, nodesNumber + 1):
+            if (j in adjacency_list[i]):
+                if (min(edges_r[(i, j)]) >= s):
+                    edges_p.append((i, j))
+            else:
+                edges_n.append((i, j))
+            if (len(edges_p) > 30000) and (len(edges_n) > 30000):
+                break
+        if (len(edges_p) > 30000) and (len(edges_n) > 30000):
+            break
+
+    # all_possible_edges = []
+    #
+    # for i in range(1, nodesNumber + 1):
+    #     for j in range(i, nodesNumber + 1):
+    #         all_possible_edges.append((i, j))
+
+    # for edge in all_possible_edges:
+    #     if (edge not in edges_r):
+    #         edges_n.append(edge)
+    #         number0 += 1
+    #     elif (min(edges_r[edge]) > s):
+    #         edges_p.append(edge)
+    #         number1 += 1
+
+    edges_p_copy = edges_p.copy()
+    edges_n_copy = edges_n.copy()
+
+    print('Посчитал edges_p и edges_n')
+    print('edges_p: ' + str(len(edges_p)))
+    print('edges_n: ' + str(len(edges_n)))
+
+    #Считаем для 4.1.1
+
+    #Ищем все пары вершин, между которыми расстояние 2
+    pair_dist_2 = []
+    for node in adjacency_list_until_s:
+        nodes_dist_2 = BFS(node, adjacency_list_until_s)
+        for node_2 in nodes_dist_2:
+            pair_dist_2.append((min(node, node_2), max(node, node_2)))
+
+    edges_p = list(set(pair_dist_2) & set(edges_p))
+    edges_n = list(set(pair_dist_2) & set(edges_n))
+    number1 = len(edges_p)
+    number0 = len(edges_n)
+
+    #Вариант через Флойда-Воршалла
+    # d = floyd_warshall(adjacency_list_until_s)
+    #
+    # seen_edges = []
+    # for i in range(0, len(d)):
+    #     for j in range(i, len(d)):
+    #         if ((min(d[i][j][1], d[i][j][2]), max(d[i][j][1], d[i][j][2])) in edges_p):
+    #             seen_edges.append((min(d[i][j][1], d[i][j][2]), max(d[i][j][1], d[i][j][2])))
+    #             if (d[i][j][0] != 2):
+    #                 edges_p.remove((min(d[i][j][1], d[i][j][2]), max(d[i][j][1], d[i][j][2])))
+    #                 number1 -= 1
+    #
+    # edges_p = [edge for edge in edges_p if edge in seen_edges]
+    #
+    # seen_edges = []
+    #
+    # for i in range(0, len(d)):
+    #     for j in range(i, len(d)):
+    #         if ((min(d[i][j][1], d[i][j][2]), max(d[i][j][1], d[i][j][2])) in edges_n):
+    #             seen_edges.append((min(d[i][j][1], d[i][j][2]), max(d[i][j][1], d[i][j][2])))
+    #             if (d[i][j][0] != 2):
+    #                 edges_n.remove((min(d[i][j][1], d[i][j][2]), max(d[i][j][1], d[i][j][2])))
+    #                 number0 -= 1
+    #
+    # edges_n = [edge for edge in edges_n if edge in seen_edges]
+
+    #Ограничения на количество предсказываемых ребер
+    if (number1 >= 10000):
+        edges_p = random.sample(edges_p, 10000)
+    if (number0 >= 10000):
+        edges_n = random.sample(edges_n, 10000)
+
+    if (len(edges_p) > 4 * len(edges_n)):
+        edges_p = random.sample(edges_p, 4 * len(edges_n))
+    elif (len(edges_p) * 4 < len(edges_n)):
+        edges_n = random.sample(edges_n, 4 * len(edges_p))
+
+    print('Посчитал все для 4.1.1')
+    print('edges_p: ' + str(len(edges_p)))
+    print('edges_n: ' + str(len(edges_n)))
+    # Считаем для 4.1.2
+    temp = tmin
+    tmin = tmax
+    tmax = temp
+
+    edges_r_until_s_multi = edges_r_until_s.copy()
+
+    #Ищем все ребра, у которых несколько временных отметок
+    for edge in edges_r_until_s:
+        edges_r_until_s_multi[edge] = list(set(edges_r_until_s[edge]))
+        if len(edges_r_until_s_multi[edge]) < 2:
+            edges_r_until_s_multi.pop(edge)
+        else:
+            for time in edges_r_until_s_multi[edge]:
+                if time > tmax:
+                    tmax = time
+                if time < tmin:
+                    tmin = time
+
+    #Создаем новый список смежности из полученных ребер
+    adjacency_list_until_s_multi = adjacency_list_until_s.copy()
+    for node in adjacency_list_until_s_multi:
+        new_neighs = []
+        for neigh in adjacency_list_until_s_multi[node]:
+            if (node, neigh) in edges_r_until_s_multi or (neigh, node) in edges_r_until_s_multi:
+                new_neighs.append(neigh)
+        adjacency_list_until_s_multi[node] = new_neighs
+
+    #Ищем пары вершин, между которыми расстояние 2 в новом списке смежности
+    pair_dist_2_multi = []
+    for node in adjacency_list_until_s_multi:
+        nodes_dist_2 = BFS(node, adjacency_list_until_s_multi)
+        for node_2 in nodes_dist_2:
+            pair_dist_2_multi.append((min(node, node_2), max(node, node_2)))
+
+    edges_p_multi = list(set(pair_dist_2_multi) & set(edges_p_copy))
+    edges_n_multi = list(set(pair_dist_2_multi) & set(edges_n_copy))
+    number1_multi = len(edges_p_multi)
+    number0_multi = len(edges_n_multi)
+
+    # Ограничения на количество предсказываемых ребер
+    if (number1_multi >= 10000):
+        edges_p_multi = random.sample(edges_p_multi, 10000)
+    if (number0_multi >= 10000):
+        edges_n_multi = random.sample(edges_n_multi, 10000)
+
+    if (len(edges_p_multi) > len(edges_n_multi)):
+        edges_p_multi = random.sample(edges_p_multi, len(edges_n_multi))
+    elif (len(edges_p_multi) < len(edges_n_multi)):
+        edges_n_multi = random.sample(edges_n_multi, len(edges_p_multi))
+
+    print('Посчитал все для 4.1.2')
+    print('edges_p_multi: ' + str(len(edges_p_multi)))
+    print('edges_n_multi: ' + str(len(edges_n_multi)))
+
+    return adjacency_list, adjacency_list_until_s, adjacency_list_until_s_multi, edges_r, edges_r_until_s, \
+           edges_r_until_s_multi, edges_p, edges_n, edges_p_multi, edges_n_multi, tmin, tmax, count_edges, is_loop
---
+++ graph_properties.py
@@ -0,0 +1,218 @@
+import pandas as pd
+import math
+import random
+import numpy as np
+from collections import deque
+
+#количество вершин
+def nodes_number(adjacency_list):
+    # adjacency_list = get_adjacency_list(data)[0]
+    return len(adjacency_list)
+
+#количество ребер
+def edges_number(count_edges):
+    return count_edges
+
+#плотность графа
+def graph_density(edges_num, nodes_num, is_loop):
+    if is_loop:
+        return edges_num / (nodes_num * (nodes_num + 1) / 2)
+    else:
+        return edges_num / (nodes_num * (nodes_num - 1) / 2)
+
+#получить все компоненты слабой связности
+def get_components(adjacency_list):
+    def DFS(node, component):
+        visited.add(node)
+        component.append(node)
+        for neighbour in adjacency_list[node]:
+            if neighbour not in visited:
+                DFS(neighbour, component)
+
+    def BFS(node, component):
+        queue = deque()
+        queue.append(node)
+        visited.add(node)
+        while len(queue) > 0:
+            cur = queue.popleft()
+            component.append(cur)
+            for neighbour in adjacency_list[cur]:
+                if (neighbour not in visited):
+                    queue.append(neighbour)
+                    visited.add(neighbour)
+
+    visited = set()
+    components = dict({})
+    count = 0
+    for node in adjacency_list:
+        if node not in visited:
+            count += 1
+            component = []
+            # DFS(node, component)
+            BFS(node, component)
+            components[count] = component
+    return components
+
+#получить количество компонент слабой связности
+def components_number(adjacency_list):
+    return(len(get_components(adjacency_list)))
+
+#получить максимальную по мощности компоненту и долю вершин в максимальной по мощности компоненте слабой связности
+def max_component( adjacency_list):
+    components = get_components(adjacency_list)
+    max_key = max(components, key = lambda x: len(components[x]))
+    return [components[max_key],len(components[max_key])/nodes_number(adjacency_list)]
+
+# получить матрицу расстояний между всеми вершинами макс компоненты(или сэмпла из макс компоненты)
+def floyd_warshall(adjacency_list, max_comp):
+    d = [[math.inf for _ in range(len(max_comp))] for __ in range(len(max_comp))]
+    for node in max_comp:
+        for neighbour in adjacency_list[node]:
+            if neighbour in max_comp:
+                d[max_comp.index(node)][max_comp.index(neighbour)] = 1
+
+    for i in range(len(max_comp)):
+        d_i = [d[j][:] for j in range(len(max_comp))]
+        for u in range(len(max_comp)):
+            for v in range(len(max_comp)):
+                d[u][v] = min(d_i[u][v], d_i[u][i] + d_i[i][v])
+
+    for i in range(len(max_comp)):
+        d[i][i] = 0
+    return d
+
+# сэмплирование методом снежный ком
+def BFS(adjacency_list, nodes):
+    visited = set(nodes)
+    sample = []
+    queue = deque()
+    queue.extend(nodes)
+
+    while (len(sample) < 500 and len(queue) > 0):
+        cur = queue.popleft()
+        sample.append(cur)
+        for neighbour in adjacency_list[cur]:
+            if (neighbour not in visited):
+                queue.append(neighbour)
+                visited.add(neighbour)
+
+    return sample
+
+#диаметр, радиус, 90 персентиль для рандомного семплирования макс компоненты
+def random_sample_metrics(adjacency_list, max_comp):
+    # adjacency_list = get_adjacency_list(data)[0]
+    # max_comp = max_component(data)[0]
+    if len(max_comp) > 500:
+        random_sample = random.sample(max_comp, 500)
+    else:
+        random_sample = max_comp
+    d = floyd_warshall(adjacency_list, random_sample)
+    dist_list = []
+    diam = -1
+    rad = math.inf
+    for i in d:
+        row_max = -1
+        for j in i:
+            if j!=math.inf:
+                dist_list.append(j)
+                if j>row_max:
+                    row_max = j
+        if row_max != -1:
+            diam = max(row_max, diam)
+            rad = min(row_max, rad)
+    return [diam, rad, np.percentile(dist_list,90)]
+
+# диаметр, радиус, 90 персентиль для семплирования методом snowball макс компоненты
+def snowball_sample(adjacency_list, max_comp):
+    # adjacency_list = get_adjacency_list(data)[0]
+    # max_comp = max_component(data)[0]
+    if len(max_comp) > 500:
+        sample = BFS(adjacency_list, random.sample(max_comp, 2))
+    else:
+        sample = max_comp
+
+    d = floyd_warshall(adjacency_list, sample)
+    dist_list = []
+    diam = -1
+    rad = math.inf
+    for i in d:
+        row_max = -1
+        for j in i:
+            if j != math.inf:
+                dist_list.append(j)
+                if j > row_max:
+                    row_max = j
+        if row_max != -1:
+            diam = max(row_max, diam)
+            rad = min(row_max, rad)
+    return [diam, rad, np.percentile(dist_list, 90)]
+
+# Для наибольшей компоненты слабой связности вычислить средний кластерный коэффициент сети
+def CL(adjacency_list, max_comp):
+    # adjacency_list = get_adjacency_list(data)[0]
+    # max_comp = max_component(data)[0]
+    cl = 0
+    for node in max_comp:
+        CLu = 0
+        loop_edges = 0
+        if (len(adjacency_list[node]) >= 2):
+            for node2 in adjacency_list[node]:
+                for node3 in adjacency_list[node2]:
+                    if node3 in adjacency_list[node]:
+                        if node3 == node2:
+                            loop_edges += 1
+                        else:
+                            CLu += 1
+            cl += (CLu + 2 * loop_edges) / (len(adjacency_list[node]) * (len(adjacency_list[node]) - 1))
+
+    return cl / (len(max_comp))
+
+#расчёт корреляции
+def r_pearson(adjacency_list):
+    # adjacency_list = get_adjacency_list(data)[0]
+    X = []
+    Y = []
+    visited = set()
+    for vertex_1 in adjacency_list:
+        for vertex_2 in adjacency_list[vertex_1]:
+            if ((vertex_2, vertex_1) not in visited) and ((vertex_1,vertex_2) not in visited):
+                X.append(len(adjacency_list[vertex_1]))
+                Y.append(len(adjacency_list[vertex_2]))
+                visited.add((vertex_1, vertex_2))
+
+    Avg_X = sum(X) / len(X)
+    Avg_Y = sum(Y) / len(Y)
+
+    deltaX = [x - Avg_X for x in X]
+    deltaY = [y - Avg_Y for y in Y]
+
+    numerator = sum([delta_x * delta_y for delta_x, delta_y in zip(deltaX, deltaY)])
+    denominator = (sum([delta_x * delta_x for delta_x in deltaX]) * sum([delta_y * delta_y for delta_y in deltaY])) ** 0.5
+    if denominator == 0:
+        return 0
+    else:
+        return numerator / denominator
+
+
+def get_graph_properties(adjacency_list, count_edges, is_loop):
+    nodes_num = nodes_number(adjacency_list)
+    edges_num = edges_number(count_edges)
+    print('Кол-во вершин: ' + str(nodes_num))
+    print('Кол-во ребер: ' + str(edges_num))
+    print('Плотность: '+ str(graph_density(edges_num, nodes_num, is_loop)))
+    print('Кол-во слабых компонент: ' + str(components_number(adjacency_list)))
+    max_comp, proportion = max_component(adjacency_list)
+    print('Мощность максимальной слабой компоненты: ' + str(len(max_comp)))
+    print('Доля вершин в максимальной слабой компоненте: ' + str(proportion))
+
+    diam_1, rad_1, percent_1 = random_sample_metrics(adjacency_list, max_comp)
+    print('Радиус: ' + str(rad_1))
+    print('Диаметр: ' + str(diam_1))
+    print('90 процентиль: ' + str(percent_1))
+
+    diam_2, rad_2, percent_2 = snowball_sample( adjacency_list, max_comp)
+    print('Радиус: ' + str(rad_2))
+    print('Диаметр: ' + str(diam_2))
+    print('90 процентиль: ' + str(percent_2))
+    print('Средний кластерный коэффициент: ' + str(CL(adjacency_list, max_comp)))
+    print('Коэффициент Пирсона: ' + str(r_pearson(adjacency_list)))
