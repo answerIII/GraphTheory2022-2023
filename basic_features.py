b17ecdba45029a21df6d29ac420dc4ab7da3bb02
Alina Smolina <78797567+Alina-Smol@users.noreply.github.com>
2023-06-07 21:16:46 +0300

    Update basic_features.py

--- basic_features.py
+++ basic_features.py
@@ -36,45 +36,23 @@ def print_basic_properties(dataset, display_interm_results = False) -> None:
 
         return WCC
 
+    graph = defaultdict(set)  # исходный граф
 
-    # count number of vertexes
-    uniqueVertexes = set()
     for line in dataset:
         [from_ind, to_ind] = [int(x) for x in line.split()]
-        uniqueVertexes.add(from_ind)
-        uniqueVertexes.add(to_ind)
-    V = len(uniqueVertexes) + 1
-
-
-    adjList = [[] for _ in range(V)]
-    # transform directed graph to undirected
-    for line in dataset:
-        [from_ind, to_ind] = [int(x) for x in line.split()]
-        adjList[from_ind].append(to_ind)
-        adjList[to_ind].append(from_ind)
-
-    # delete duplicate edges
-    for i in range(V):
-        _set = set(adjList[i])
-        adjList[i] = list(_set)
-
-    if display_interm_results:
-        print("uniqueVertexes:" , uniqueVertexes)
-        print("adjList:")
-        for i in range(V):
-            print(i, ":", end=" ")
-            print(adjList[i])
+        graph[from_ind].add(to_ind)
+        graph[to_ind].add(from_ind)
 
 
     sum_E = 0
-    for _set in adjList:
-        sum_E+=len(_set)
+    for i in graph.items():
+        sum_E += len(i[1])
     E = sum_E // 2
 
     # максимальное число ребер в полном графе с количеством вершин V - 1
-    max_E = (V - 1)*(V - 2) // 2
+    max_E = (len(graph))*(len(graph) - 1) // 2
 
-    print("Number of vertexes:", V - 1)
+    print("Number of vertexes:", len(graph))
     print("Number of edges:", E)
     print("Maximal number of edges:", max_E)
     print("Density:", E / max_E)
@@ -82,8 +60,9 @@ def print_basic_properties(dataset, display_interm_results = False) -> None:
     count = 0
     max_size = 0
     max_WCC = set()
+    uniqueVertexes = list(graph.keys())
     while len(uniqueVertexes) > 0:
-        WCC = bfs(adjList, uniqueVertexes)
+        WCC = bfs(graph, uniqueVertexes)
         size = len(WCC)
         if size > max_size:
             max_size = size
@@ -96,7 +75,7 @@ def print_basic_properties(dataset, display_interm_results = False) -> None:
     if display_interm_results: 
         print("Наибольшая КСС: ", max_WCC)
     print("Мощность наибольшей КСС: ", max_size)
-    print("Отношение мощности наибольшей КСС к общему количеству вершин: ", max_size / (V - 1))
+    print("Отношение мощности наибольшей КСС к общему количеству вершин: ", max_size / len(graph))
 
     
     ### Пункт 1.2.
@@ -111,13 +90,13 @@ def print_basic_properties(dataset, display_interm_results = False) -> None:
         
     #print("подграф с наибольшей КСС: ", ver) 
 
-    n = 1000 # кол-во вершин для 2a 
+    n = len(ver) # кол-во вершин для 2a
 
     # random_v = random.choices(list(max_WCC), k=n) 
     #random_v =random.sample(list(max_WCC), k=n) # мн-во вершин для 2a
     #print('random_v', random_v)
-
-    def dijkstra_algo(adjList: list[list], start) -> list:
+    V = len(graph) + 1
+    def dijkstra_algo(adjList, start) -> list:
       min_queue = queue.PriorityQueue()
       distance = [np.inf] * V
 
@@ -144,7 +123,7 @@ def print_basic_properties(dataset, display_interm_results = False) -> None:
             # print()
 
       # print('k',distance) 
-      distance[0]=0
+      distance[0] = 0
       if np.inf in distance: 
         distance[distance.index(np.inf)] = 0 
       # print(distance)     
@@ -163,13 +142,13 @@ def print_basic_properties(dataset, display_interm_results = False) -> None:
         return e, e_1
 
     #print(ver)
+    print("max_WCC", max_WCC)
+    eccentricity = find_eccentrisity(ver, max_WCC)
+    print('-------')
 
-    # eccentricity = find_eccentrisity(ver, random_v)
-    # print('-------') 
-
-    # print("Диаметр:", max(eccentricity[0]), "Совпадает со встроенной ф-цией?") 
-    # print("Радиус:", min(eccentricity[0]), "Совпадает со встроенной ф-цией?") 
-    # print("90 процентиля расстояния (геодезического) между вершинами графа:", np.percentile(eccentricity[1], 90)) 
+    print("Диаметр:", max(eccentricity[0]), "Совпадает со встроенной ф-цией?")
+    print("Радиус:", min(eccentricity[0]), "Совпадает со встроенной ф-цией?")
+    print("90 процентиля расстояния (геодезического) между вершинами графа:", np.percentile(eccentricity[1], 90))
 
 
     def bfs_snowball(visited, adjList, unvisited, lenght):
@@ -198,44 +177,49 @@ def print_basic_properties(dataset, display_interm_results = False) -> None:
             size = len(new_WCC)
             if size > max_size:
                 max_size = size
-                max_WCC = new_WCC
+                new_max_WCC = new_WCC
 
         #print("snow-ball component", max_WCC)
-        for v in max_WCC:
+        for v in new_max_WCC:
             for neighbour_node in adjList[v]:
-                if neighbour_node in max_WCC:
+                if neighbour_node in new_max_WCC:
                     edges[v].add(neighbour_node)
                     edges[neighbour_node].add(v)
 
-        return edges, max_WCC
+        return edges, new_max_WCC
 
 
 
 
-    # random_v = random.sample(list(max_WCC), k=2)
-    # edges, new_WCC = bfs_snowball(random_v, adjList, max_WCC, n)
-    # #print("Snowball: ", edges)
+    random_v = random.sample(list(max_WCC), k=2)
+
+    print('max_WCC', max_WCC)
+
+    # max_WCC_1 = max_WCC
+    # edges, new_WCC = bfs_snowball(random_v, graph, max_WCC_1, len(max_WCC_1))
+    # # #print("Snowball: ", edges)
     # print("Size snowball component: ", len(new_WCC))
-
+    # ###############################################
+    # print('max_WCC', max_WCC)
     # matrix_of_shortest_paths = find_eccentrisity(edges, new_WCC)
-
+    #
     # print("Диаметр snowball:", max(matrix_of_shortest_paths[0]))
     # print("Радиус snowball:", min(matrix_of_shortest_paths[0]))
-    # print("90 процентиля расстояния (геодезического) между вершинами графа snowball:", np.percentile(matrix_of_shortest_paths[1], 90))    
+    # print("90 процентиля расстояния (геодезического) между вершинами графа snowball:", np.percentile(matrix_of_shortest_paths[1], 90))
 
 
 
     ### Пункт 1.3. Посчитать средний кластерный коэффициент для наибольшей КСС
-
+    #
     def local_clustering_coefficient(adjList, u) -> float:
-        
+
         # сет соседей вершины u
         adj2u_set = set()
         for v in adjList[u]:
             adj2u_set.add(v)
 
         gamma_u = len(adj2u_set)
-        
+
         if (gamma_u < 2):
             return 0
 
@@ -258,7 +242,7 @@ def print_basic_properties(dataset, display_interm_results = False) -> None:
 
     sum_Cl = 0
     for vertex in max_WCC:
-        sum_Cl+=local_clustering_coefficient(adjList, vertex)
+        sum_Cl+=local_clustering_coefficient(graph, vertex)
 
     average_Cl = sum_Cl / len(max_WCC)
     print("Средний кластерный коэффициент для наибольшей КСС:", average_Cl)
@@ -270,13 +254,13 @@ def print_basic_properties(dataset, display_interm_results = False) -> None:
     def pearson_correlation_coefficient_by_degree(adjList, edge_list):
         X = []
         Y = []
-        
+
         for edge in edge_list:
             x = edge[0]
             y = edge[1]
             X.append(len(adjList[x]))
             Y.append(len(adjList[y]))
-        
+
         avg_x = sum(X) / len(X)
         avg_y = sum(Y) / len(Y)
 
@@ -309,7 +293,7 @@ def print_basic_properties(dataset, display_interm_results = False) -> None:
 
 
     # Сравнение результатов с алгоритмом из библиотеки networkx
-    my_r = pearson_correlation_coefficient_by_degree(adjList, edge_list)
+    my_r = pearson_correlation_coefficient_by_degree(graph, edge_list)
     print("my_pearson_correlation_coefficient_by_degree:", my_r)
 
     G = nx.Graph(edge_list)
@@ -318,4 +302,13 @@ def print_basic_properties(dataset, display_interm_results = False) -> None:
 
     print("error: ", abs(nx_r - my_r))
 
+#1.2b snowball
+    max_WCC_1 = max_WCC
+    edges, new_WCC = bfs_snowball(random_v, graph, max_WCC_1, len(max_WCC_1))
+    # #print("Snowball: ", edges)
+    print("Size snowball component: ", len(new_WCC))
+    matrix_of_shortest_paths = find_eccentrisity(edges, new_WCC)
 
+    print("Диаметр snowball:", max(matrix_of_shortest_paths[0]))
+    print("Радиус snowball:", min(matrix_of_shortest_paths[0]))
+    print("90 процентиля расстояния (геодезического) между вершинами графа snowball:", np.percentile(matrix_of_shortest_paths[1], 90))
